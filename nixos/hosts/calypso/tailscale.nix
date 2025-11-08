dd3106ba4536bc655630b1c3bf7317a918038067
Nicolas Dufour <nrdufour@gmail.com>
2025-11-08 09:53:59 -0500

    fix(calypso): do not accept outside routes for tailscale

--- nixos/hosts/calypso/tailscale.nix
+++ nixos/hosts/calypso/tailscale.nix
@@ -8,6 +8,22 @@
     authKeyFile = config.sops.secrets."tailscale_auth_key".path;
   };
 
+  # Configure Tailscale settings after startup
+  systemd.services.tailscale-config = {
+    description = "Configure Tailscale to reject subnet routes";
+    after = [ "tailscaled.service" ];
+    wants = [ "tailscaled.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = pkgs.writeShellScript "tailscale-config" ''
+        # Don't accept subnet routes from other nodes
+        ${pkgs.tailscale}/bin/tailscale set --accept-routes=false
+      '';
+    };
+  };
+
   # Open firewall for Tailscale
   networking.firewall = {
     allowedUDPPorts = [ 41641 ]; # Tailscale port
