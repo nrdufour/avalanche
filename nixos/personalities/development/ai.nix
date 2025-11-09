139e1e53594a7564458d13b2bb7ec882c45808cb
Nicolas Dufour <nrdufour@gmail.com>
2025-11-09 14:03:44 -0500

    fix(calypso): permissions issues for open-webui - take 3

--- nixos/personalities/development/ai.nix
+++ nixos/personalities/development/ai.nix
@@ -32,6 +32,7 @@
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";
       SCARF_NO_ANALYTICS = "True";
+      DATA_DIR = "/var/lib/open-webui";
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
       OLLAMA_BASE_URL = "http://127.0.0.1:11434";
     };
